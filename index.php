37a35a1e28641cb183683fa82169f65a72fbdd68
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2024-09-20 21:14:04 -0500

    Enviar index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Programación Orientada a Objetos</h1>
+    <h2>Docente</h2>
+    <?php
+    require('./process/objeto_docente.php')
+    ?>
+    <h2>Alumno</h2>
+    <?php
+    require('./process/objeto_alumno.php')
+    ?>
+</body>
+</html>

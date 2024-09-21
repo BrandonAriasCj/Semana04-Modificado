9047a909a5024fa7d2b249697673269596cf7cc7
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2024-09-20 21:32:45 -0500

    Se agrego los archivos "index", "agregarAlumnos", "agregarProfesores"

--- index.php
+++ index.php
@@ -1,19 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tareas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>Programación Orientada a Objetos</h1>
-    <h2>Docente</h2>
-    <?php
-    require('./process/objeto_docente.php')
-    ?>
-    <h2>Alumno</h2>
-    <?php
-    require('./process/objeto_alumno.php')
-    ?>
+    <h1>Agregar Datos</h1>
+    <br>
+    <div class="container">
+        <h2>Datos de Alumnos</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Datos</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+        </table>
+        <a href="agregarAlumno.html" class="btn btn-primary">Agregar Alumno</a>
+    </div>
+    <br>
+    <br>
+    <div class="container">
+        <h2>Datos de Profesores</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Datos</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+        </table>
+        <a href="agregarProfesor.html" class="btn btn-primary">Agregar Profesor</a>
+</button>
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
